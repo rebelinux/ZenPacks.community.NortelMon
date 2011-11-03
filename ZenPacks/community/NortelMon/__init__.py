00a3a5e6281f3b79afba1e8c7beb38b5fc9231cd
Jonathan Colon <rebelinux@gmail.com>
2011-11-02 21:23:45 -0400

    Add BuildRelation on Zenpack Instalation and Removal + Templates fixes

--- ZenPacks/community/NortelMon/__init__.py
+++ ZenPacks/community/NortelMon/__init__.py
@@ -1,8 +1,26 @@
-
 import Globals
 import os.path
+from Products.ZenModel.ZenPack import ZenPackBase
 
 skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
 from Products.CMFCore.DirectoryView import registerDirectory
 if os.path.isdir(skinsDir):
     registerDirectory(skinsDir, globals())
+
+class ZenPack(ZenPackBase):
+    """ ZenPack loader
+"""
+    def install(self, app):
+        ZenPackBase.install(self, app)
+        for d in self.dmd.Devices.getSubDevices():
+            d.buildRelations()
+
+    def upgrade(self, app):
+        ZenPackBase.upgrade(self, app)
+        for d in self.dmd.Devices.getSubDevices():
+            d.buildRelations()
+
+    def remove(self, app, leaveObjects=False):
+        ZenPackBase.remove(self, app, leaveObjects)
+        for d in self.dmd.Devices.getSubDevices():
+            d.buildRelations()
