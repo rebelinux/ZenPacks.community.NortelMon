fb89e3479957e58f3d823054c824230ba60da7ac
Jonathan Colon <rebelinux@gmail.com>
2012-01-09 15:46:47 -0400

    Release 1.4.7.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.NortelMon"
-VERSION = "1.4.7.1"
+VERSION = "1.4.7.2"
 AUTHOR = "Jonathan Colon"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
